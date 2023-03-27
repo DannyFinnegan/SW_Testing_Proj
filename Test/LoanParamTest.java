f91fae0da17a862df8bfe4a2034063be49d1d5a3
t00225685 <daniel.finnegan@students.ittralee.ie>
2023-03-27 10:34:40 +0100

    Updated param

--- Test/LoanParamTest.java
+++ Test/LoanParamTest.java
@@ -22,6 +22,39 @@ public class LoanParamTest {
         Loan loan = new Loan(amount, period);
         assertEquals(answer, loan.getTotalPayment());
     }
+    @ParameterizedTest
+    @CsvFileSource(resources = "ParamTestFileMonthlyNegative.csv", numLinesToSkip = 1)
+    public void TestMonthlyFails( String Period, String Amount) {
+        assertThrows(IllegalArgumentException.class, () -> {Loan loan = new Loan( Double.parseDouble(Amount), Integer.parseInt(Period));
+        });
+    }
+    @ParameterizedTest
+    @CsvFileSource(resources = "ParamTestFileTotalNegative.csv", numLinesToSkip = 1)
+    public void TestTotalFails( String Period, String Amount) {
+        assertThrows(IllegalArgumentException.class, () -> {Loan loan = new Loan( Double.parseDouble(Amount), Integer.parseInt(Period));
+        });
+    }
+    @ParameterizedTest
+    @CsvFileSource(resources = "ParamTestGetRate.csv", numLinesToSkip = 1)
+    public void TestGetRate (double amount, int period, double answer){
+        Loan loan = new Loan(amount, period);
+        assertEquals(answer,loan.getRate());
 
+    }
+    @ParameterizedTest
+    @CsvFileSource(resources = "ParamTestGetPeriod.csv", numLinesToSkip = 1)
+    public void TestGetPeriod (double amount, int period, double answer){
+        Loan loan = new Loan(amount, period);
+        assertEquals(answer,loan.getPeriod());
+
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "ParamTestGetAmount.csv", numLinesToSkip = 1)
+    public void TestGetAmount (double amount, int period, double answer){
+        Loan loan = new Loan(amount, period);
+        assertEquals(answer,loan.getAmount());
+
+    }
 
 }

fbbe4f54c88c70b1510df6014c389e438fda8bb5
t00225685 <daniel.finnegan@students.ittralee.ie>
2023-03-27 09:27:04 +0100

    Updated param

--- Test/LoanParamTest.java
+++ Test/LoanParamTest.java
@@ -10,14 +10,14 @@ public class LoanParamTest {
     private int period;
 
     @ParameterizedTest
-    @CsvFileSource(resources = "ParamTestFileMonthly.csv", numLinesToSkip = 1)
+    @CsvFileSource(resources = "ParamTestFileMonthlyPositive.csv", numLinesToSkip = 1)
     void testcheckMontlyRateAll(double amount, int period, double answer) {
         Loan loan = new Loan(amount, period);
         assertEquals(answer, loan.getMonthlyPayment());
     }
 
     @ParameterizedTest
-    @CsvFileSource(resources = "ParamTestFileTotal.csv", numLinesToSkip = 1)
+    @CsvFileSource(resources = "ParamTestFileTotalPositive.csv", numLinesToSkip = 1)
     void testCheckTotalPaymentAll(double amount, int period, double answer) {
         Loan loan = new Loan(amount, period);
         assertEquals(answer, loan.getTotalPayment());
